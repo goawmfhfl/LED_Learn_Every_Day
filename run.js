1e437c011413a4495de5859394daa8a887205691
jy <goawmfhfl@gmail.com>
2021-09-14 21:27:50 +0900

    [0914]DeepDive.js

--- run.js
+++ run.js
@@ -1,17 +1,25 @@
-function solution(arr) {
-    let answer = arr
-    for(let i=1; i<arr.length; i++){
-        let tmp = arr[i];
-        let j;
-        for(j=i-1; j>=0; j--){
-            if(arr[j]>tmp) arr[j+1]=arr[j]
-            else break;
+function solution(arr,n) {
+    let answer = Array.from({length:n},(v)=> v = 0)
+    arr.forEach(x => {
+        let pos = -1;
+        for(let i=0; i<n; i++) if(x===arr[i]) pos=i
+        if(pos === -1){
+            for(let i=n-1; j>=1; j--){
+                arr[i]=arr[i-1]
+            }
+        }else{
+            for(let i=pos; i>=1; j--){
+                arr[i]=arr[i-1]
+            }
         }
-        arr[j+1]=tmp
-    }
-    return answer
+        answer[0]=x
+    });
+
+    return answer;
 }
 
 
-let arr = [11,7,5,6,10,9]
-console.log(solution(arr)); 
+let arr = [1,2,3,2,6,2,3,5,7]
+let n = 5
+
+console.log(solution(arr,n));
