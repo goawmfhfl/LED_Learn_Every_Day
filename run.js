06421526c71312381416542f912578fdcb083a11
jy <goawmfhfl@gmail.com>
2021-09-15 19:04:17 +0900

    [0915]Study

--- run.js
+++ run.js
@@ -1,25 +1,14 @@
-function solution(arr,n) {
-    let answer = Array.from({length:n},(v)=> v = 0)
-    arr.forEach(x => {
-        let pos = -1;
-        for(let i=0; i<n; i++) if(x===arr[i]) pos=i
-        if(pos === -1){
-            for(let i=n-1; j>=1; j--){
-                arr[i]=arr[i-1]
-            }
-        }else{
-            for(let i=pos; i>=1; j--){
-                arr[i]=arr[i-1]
-            }
-        }
-        answer[0]=x
-    });
+function solution(arr) {
+    let plus = []
+    let minus = []
 
-    return answer;
-}
+    for(let x of arr){
+        if(x>0) plus.push(x)
+        else minus.push(x)
+    }
 
-
-let arr = [1,2,3,2,6,2,3,5,7]
-let n = 5
-
-console.log(solution(arr,n));
+    return [...minus,...plus]
+  }
+  
+  let arr = [1,2,3,-3,-2,5,6,-6];
+  console.log(solution(arr));
